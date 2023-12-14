581103343f2339cf8687733be290c032a03737f6
brian-nunez <bnunez9999@gmail.com>
2023-12-14 15:08:53 -0700

    fix(config): update info

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://finance.bjnunez.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/finance-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
